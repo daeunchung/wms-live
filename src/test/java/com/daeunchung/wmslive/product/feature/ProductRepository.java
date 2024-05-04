8a514dc1b7061bb3c2548c27ae0baaa410498141
Daeun Chung <46639550+daeunchung@users.noreply.github.com>
2024-05-05 01:29:34 +0900

    5. move inner classes to upper level

---
+++ src/test/java/com/daeunchung/wmslive/product/feature/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.daeunchung.wmslive.product.feature;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class ProductRepository {
+    private final Map<Long, Product> products = new HashMap<>();
+    private Long nextId = 1L;
+
+    public void save(final Product product) {
+        product.assignId(nextId);
+        nextId++;
+        products.put(product.getId(), product);
+    }
+
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
+    }
+}
