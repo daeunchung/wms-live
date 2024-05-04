dfdd74299762eab42cda3b30205860ee2d8bcb2c
Daeun Chung <46639550+daeunchung@users.noreply.github.com>
2024-05-04 20:42:07 +0900

    2. create test case RegisterProduct

--- src/test/java/com/daeunchung/wmslive/product/feature/RegisterProductTest.java
+++ src/test/java/com/daeunchung/wmslive/product/feature/RegisterProductTest.java
@@ -1,7 +1,16 @@
 package com.daeunchung.wmslive.product.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 /**
  * 실무에서는 레이어드 아키텍쳐 형태로 개발을 해야하지만, 예제 실습 코드라서 클래스 형태로 생성하겠습니다. 실무에서는 권장하지 않음을 기억해주세요
  */
 class RegisterProductTest {
+
+    @Test
+    @DisplayName("상품을 등록한다.")
+    void registerProduct() {
+
+    }
 }
