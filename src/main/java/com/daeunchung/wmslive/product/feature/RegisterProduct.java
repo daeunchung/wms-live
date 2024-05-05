26b8ca76e73ea58385fd38c788c3a5344debf949
Daeun Chung <46639550+daeunchung@users.noreply.github.com>
2024-05-05 16:14:31 +0900

    8.RegisterProduct API 로 변경

--- src/main/java/com/daeunchung/wmslive/product/feature/RegisterProduct.java
+++ src/main/java/com/daeunchung/wmslive/product/feature/RegisterProduct.java
@@ -1,8 +1,16 @@
 package com.daeunchung.wmslive.product.feature;
 
 import com.daeunchung.wmslive.product.domain.*;
-import org.springframework.util.Assert;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class RegisterProduct {
     private final ProductRepository productRepository;
 
@@ -10,50 +18,65 @@ public class RegisterProduct {
         this.productRepository = productRepository;
     }
 
-    public void request(final Request request) {
+    @PostMapping("/products")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void request(@RequestBody final Request request) {
         final Product product = request.toDomain();
         productRepository.save(product);
     }
 
     public record Request(
+            // Request (API 요청값)의 validation check 를 jakarta 로 변경
+            @NotBlank(message = "상품명은 필수입니다.")
             String name,
+            @NotBlank(message = "상품명은 필수입니다.")
             String code,
+            @NotBlank(message = "상품명은 필수입니다.")
             String description,
+            @NotBlank(message = "상품명은 필수입니다.")
             String brand,
+            @NotBlank(message = "상품명은 필수입니다.")
             String maker,
+            @NotBlank(message = "상품명은 필수입니다.")
             String origin,
+            @NotNull(message = "카테고리는 필수입니다.")
             Category category,
+            @NotNull(message = "온도대는 필수입니다.")
             TemperatureZone temperatureZone,
+            @NotNull(message = "상품의 무게는 필수입니다.") @Min(value = 0, message = "상품의 무게는 0보다 작을 수 없습니다.")
             Long weightInGrams,
+            @NotNull(message = "상품의 너비는 필수입니다.") @Min(value = 0, message = "상품의 무게는 0보다 작을 수 없습니다.")
             Long widthInMillimeters,
+            @NotNull(message = "상품의 높이는 필수입니다.") @Min(value = 0, message = "상품의 무게는 0보다 작을 수 없습니다.")
             Long heightInMillimeters,
+            @NotNull(message = "상품의 길이는 필수입니다.") @Min(value = 0, message = "상품의 무게는 0보다 작을 수 없습니다.")
             Long lengthInMillimeters) {
 
         public Request {
-            Assert.hasText(name, "상품명은 필수입니다.");
-            Assert.hasText(code, "상품코드는 필수입니다.");
-            Assert.hasText(description, "상품설명은 필수입니다.");
-            Assert.hasText(brand, "브랜드는 필수입니다.");
-            Assert.hasText(maker, "제조사는 필수입니다.");
-            Assert.hasText(origin, "원산지는 필수입니다.");
-            Assert.notNull(category, "카테고리는 필수입니다.");
-            Assert.notNull(temperatureZone, "온도대는 필수입니다.");
-            Assert.notNull(weightInGrams, "무게는 필수입니다.");
-            if (0 > weightInGrams) {
-                throw new IllegalArgumentException("무게는 0보다 작을 수 없습니다.");
-            }
-            Assert.notNull(widthInMillimeters, "가로길이는 필수입니다.");
-            if (0 > widthInMillimeters) {
-                throw new IllegalArgumentException("가로길이는 0보다 작을 수 없습니다.");
-            }
-            Assert.notNull(heightInMillimeters, "세로길이는 필수입니다.");
-            if (0 > heightInMillimeters) {
-                throw new IllegalArgumentException("세로길이는 0보다 작을 수 없습니다.");
-            }
-            Assert.notNull(lengthInMillimeters, "세로길이는 필수입니다.");
-            if (0 > lengthInMillimeters) {
-                throw new IllegalArgumentException("세로길이는 0보다 작을 수 없습니다.");
-            }
+//            Assert.hasText(name, "상품명은 필수입니다.");
+//            Assert.hasText(code, "상품코드는 필수입니다.");
+//            Assert.hasText(description, "상품설명은 필수입니다.");
+//            Assert.hasText(brand, "브랜드는 필수입니다.");
+//            Assert.hasText(maker, "제조사는 필수입니다.");
+//            Assert.hasText(origin, "원산지는 필수입니다.");
+//            Assert.notNull(category, "카테고리는 필수입니다.");
+//            Assert.notNull(temperatureZone, "온도대는 필수입니다.");
+//            Assert.notNull(weightInGrams, "무게는 필수입니다.");
+//            if (0 > weightInGrams) {
+//                throw new IllegalArgumentException("무게는 0보다 작을 수 없습니다.");
+//            }
+//            Assert.notNull(widthInMillimeters, "가로길이는 필수입니다.");
+//            if (0 > widthInMillimeters) {
+//                throw new IllegalArgumentException("가로길이는 0보다 작을 수 없습니다.");
+//            }
+//            Assert.notNull(heightInMillimeters, "세로길이는 필수입니다.");
+//            if (0 > heightInMillimeters) {
+//                throw new IllegalArgumentException("세로길이는 0보다 작을 수 없습니다.");
+//            }
+//            Assert.notNull(lengthInMillimeters, "세로길이는 필수입니다.");
+//            if (0 > lengthInMillimeters) {
+//                throw new IllegalArgumentException("세로길이는 0보다 작을 수 없습니다.");
+//            }
         }
 
         public Product toDomain() {
