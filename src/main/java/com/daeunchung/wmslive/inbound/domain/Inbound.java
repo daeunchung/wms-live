b79781e01872c146a95da3e73b07b9afa809e98d
Daeun Chung <46639550+daeunchung@users.noreply.github.com>
2024-05-06 20:39:20 +0900

    9.Inbound JPA 변환

--- src/main/java/com/daeunchung/wmslive/inbound/domain/Inbound.java
+++ src/main/java/com/daeunchung/wmslive/inbound/domain/Inbound.java
@@ -1,19 +1,39 @@
 package com.daeunchung.wmslive.inbound.domain;
 
-import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "inbound")
+@Comment("입고")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Inbound {
-    @Getter
-    private Long id;
-    private final String title;
-    private final String description;
-    private final LocalDateTime orderRequestedAt;
-    private final LocalDateTime estimatedArrivalAt;
-    private final List<InboundItem> inboundItems;
+    @OneToMany(mappedBy = "inbound", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private final List<InboundItem> inboundItems = new ArrayList<>();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inbound_no")
+    @Comment("입고 번호")
+    private Long inboundNo;
+    @Column(name = "title", nullable = false)
+    @Comment("입고명")
+    private String title;
+    @Column(name = "description", nullable = false)
+    @Comment("입고설명")
+    private String description;
+    @Column(name = "order_requested_at", nullable = false)
+    @Comment("입고요청일시")
+    private LocalDateTime orderRequestedAt;
+    @Column(name = "estimated_arrival_at", nullable = false)
+    @Comment("입고예정일시")
+    private LocalDateTime estimatedArrivalAt;
 
     public Inbound(
             final String title,
@@ -31,7 +51,11 @@ public class Inbound {
         this.description = description;
         this.orderRequestedAt = orderRequestedAt;
         this.estimatedArrivalAt = estimatedArrivalAt;
-        this.inboundItems = inboundItems;
+//        this.inboundItems = inboundItems;
+        for (final InboundItem inboundItem : inboundItems) {
+            this.inboundItems.add(inboundItem);
+            inboundItem.assignInbound(this);
+        }
     }
 
     private void validateConstructor(
@@ -46,9 +70,4 @@ public class Inbound {
         Assert.notNull(estimatedArrivalAt, "입고 예정일은 필수입니다.");
         Assert.notEmpty(inboundItems, "입고 품목은 필수입니다.");
     }
-
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
 }
