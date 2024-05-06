9d34302aff72e74d34981c0812ecfd9143f599f1
Daeun Chung <46639550+daeunchung@users.noreply.github.com>
2024-05-06 20:08:26 +0900

    7.Api test로 변경

--- src/main/java/com/daeunchung/wmslive/inbound/feature/RegisterInbound.java
+++ src/main/java/com/daeunchung/wmslive/inbound/feature/RegisterInbound.java
@@ -4,21 +4,29 @@ import com.daeunchung.wmslive.inbound.domain.Inbound;
 import com.daeunchung.wmslive.inbound.domain.InboundItem;
 import com.daeunchung.wmslive.inbound.domain.InboundRepository;
 import com.daeunchung.wmslive.product.domain.ProductRepository;
-import org.springframework.util.Assert;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@RestController
+@RequiredArgsConstructor
 class RegisterInbound {
     private final ProductRepository productRepository;
     private final InboundRepository inboundRepository;
 
-    RegisterInbound(final ProductRepository productRepository, final InboundRepository inboundRepository) {
-        this.productRepository = productRepository;
-        this.inboundRepository = inboundRepository;
-    }
-
-    public void request(final Request request) {
+    @PostMapping("/inbounds")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void request(@RequestBody @Valid final Request request) {
         // TODO 요청을 도메인으로 변경해서 도메인을 저장한다.
         final Inbound inbound = createInbound(request);
         inboundRepository.save(inbound);
@@ -50,36 +58,40 @@ class RegisterInbound {
     }
 
     public record Request(
-            String title,
-            String description,
-            LocalDateTime orderRequestedAt,
-            LocalDateTime estimatedArrivalAt,
-            List<Request.Item> inboundItems) {
-        public Request {
-            Assert.hasText(title, "입고 제목은 필수입니다.");
-            Assert.hasText(description, "입고 설명은 필수입니다.");
-            Assert.notNull(orderRequestedAt, "입고 요청일은 필수입니다.");
-            Assert.notNull(estimatedArrivalAt, "입고 예정일은 필수입니다.");
-            Assert.notEmpty(inboundItems, "입고 품목은 필수입니다.");
-        }
+            @NotBlank(message = "입고 제목은 필수입니다.") String title,
+            @NotBlank(message = "입고 설명은 필수입니다.") String description,
+            @NotNull(message = "입고 요청일은 필수입니다.") LocalDateTime orderRequestedAt,
+            @NotNull(message = "입고 예정일은 필수입니다.") LocalDateTime estimatedArrivalAt,
+            @NotNull(message = "입고 품목은 필수입니다.") List<Request.Item> inboundItems) {
+
+//        public Request {
+//            Assert.hasText(title, "입고 제목은 필수입니다.");
+//            Assert.hasText(description, "입고 설명은 필수입니다.");
+//            Assert.notNull(orderRequestedAt, "입고 요청일은 필수입니다.");
+//            Assert.notNull(estimatedArrivalAt, "입고 예정일은 필수입니다.");
+//            Assert.notEmpty(inboundItems, "입고 품목은 필수입니다.");
+//        }
 
         public record Item(
-                Long productNo,
-                Long quantity,
-                Long unitPrice,
-                String description) {
-            public Item {
-                Assert.notNull(productNo, "상품 번호는 필수입니다.");
-                Assert.notNull(quantity, "수량은 필수입니다.");
-                if (1 > quantity) {
-                    throw new IllegalArgumentException("수량은 1개 이상이어야 합니다.");
-                }
-                Assert.notNull(unitPrice, "단가는 필수입니다.");
-                if (0 > unitPrice) {
-                    throw new IllegalArgumentException("단가는 0원 이상이어야 합니다.");
-                }
-                Assert.hasText(description, "품목 설명은 필수입니다.");
-            }
+                @NotNull(message = "상품 번호는 필수입니다.") Long productNo,
+                @NotNull(message = "수량은 필수입니다.") @Min(value = 1, message = "수량은 1개 이상이어야 합니다.") Long quantity,
+                @NotNull(message = "단가는 필수입니다.") @Min(value = 0, message = "단가는 0원 이상이어야 합니다.") Long unitPrice,
+                @NotBlank(message = "품목 설명은 필수입니다.") String description) {
+
+            // 서비스 함수 구현 단계에서는 Spring Aseert.함수() 를 사용하다가, Api 요청 보내는 부분 개발 할때 jakarta validation 으로 전부 변경해준다.
+
+//            public Item {
+//                Assert.notNull(productNo, "상품 번호는 필수입니다.");
+//                Assert.notNull(quantity, "수량은 필수입니다.");
+//                if (1 > quantity) {
+//                    throw new IllegalArgumentException("수량은 1개 이상이어야 합니다.");
+//                }
+//                Assert.notNull(unitPrice, "단가는 필수입니다.");
+//                if (0 > unitPrice) {
+//                    throw new IllegalArgumentException("단가는 0원 이상이어야 합니다.");
+//                }
+//                Assert.hasText(description, "품목 설명은 필수입니다.");
+//            }
         }
     }
 }
--- src/test/java/com/daeunchung/wmslive/inbound/feature/RegisterInboundTest.java
+++ src/test/java/com/daeunchung/wmslive/inbound/feature/RegisterInboundTest.java
@@ -1,30 +1,40 @@
 package com.daeunchung.wmslive.inbound.feature;
 
+import com.daeunchung.wmslive.common.ApiTest;
 import com.daeunchung.wmslive.inbound.domain.InboundRepository;
 import com.daeunchung.wmslive.product.domain.ProductRepository;
-import org.junit.jupiter.api.BeforeEach;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.daeunchung.wmslive.product.fixture.ProductFixture.aProduct;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.http.HttpStatus.CREATED;
 
-class RegisterInboundTest {
+class RegisterInboundTest extends ApiTest { // 7.API TEST 로 변경
 
-    private RegisterInbound registerInbound;
+//    private RegisterInbound registerInbound;
+
+    @MockBean
     private ProductRepository productRepository;
+    @Autowired
     private InboundRepository inboundRepository;
 
-    @BeforeEach
-    void setUp() {
-        productRepository = Mockito.mock(ProductRepository.class);
-        inboundRepository = new InboundRepository();
-        registerInbound = new RegisterInbound(productRepository, inboundRepository);
-    }
+//    @BeforeEach
+//    void setUp2() {
+//        // ApiTest 에 있는 @BeforeEach 도 함수명이 setUp() 인 바람에 메서드 오버라이딩이 일어나서 ApiTest 의 setUp 이 RegisterInboundTest 의 setUp 으로 바뀌어서 덮어써진다 -> 랜덤포트 적용이 되지 않는다. 따라서 함수명을 변경해주었음
+//        productRepository = Mockito.mock(ProductRepository.class);
+//        inboundRepository = new InboundRepository();
+////        registerInbound = new RegisterInbound(productRepository, inboundRepository);
+//    }
 
     @Test
     @DisplayName("입고를 등록한다.")
@@ -53,7 +63,18 @@ class RegisterInboundTest {
                 estimatedArrivalAt,
                 inboundItems
         );
-        registerInbound.request(request);
+
+        RestAssured.given().log().all() // HTTP 요청이 콘솔에 찍히도록 설정한다
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/inbounds")
+                .then().log().all().statusCode(CREATED.value());
+
+//        registerInbound.request(request); // ApiTest 로 변경
+
+        // 검증
+        Assertions.assertThat(inboundRepository.findAll()).hasSize(1);
     }
 
 }
